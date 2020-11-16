5a91db6033452b41e9894e5685c3973b88e7950f
Palloma Gualter <pallomagualter@gmail.com>
2020-11-16 16:59:37 -0300

    teste

--- src/Show.js
+++ src/Show.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Show from './src/Show';
 
-export default function App() {
+
+export default function Show() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>TESTE!</Text>
-      <Show />
+      <Text style={styles.text}>Firebase!</Text>
     </View>
 
   );
